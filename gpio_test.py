1d93adeeb00f0a07e7abb3c263865167cf26abcd
Vadym Volokitin <seagsm.trash@gmail.com>
2016-09-17 16:14:03 +0200

    fist test

--- gpio_test.py
+++ gpio_test.py
@@ -15,6 +15,7 @@ for i in range(10):
     time.sleep(1)
     RPIO.output(NRF_CE, 0)
     time.sleep(1)
+    
 # reset every channel that has been set up by this program,
 # and unexport interrupt gpio interfaces
 RPIO.cleanup()
