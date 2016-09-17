3f46fa52c72321b6dc6bc29647341b58c41f54bf
Vadym Volokitin <seagsm.trash@gmail.com>
2016-09-17 15:51:27 +0200

    _init_.py removed

--- gpio_test.py
+++ gpio_test.py
@@ -1,17 +1,17 @@
-#import RPIO
+import RPIO
 import time
 
 NRF_CE = 24
 
 # set up output channel with an initial state
-#RPIO.setup(NRF_CE, RPIO.OUT, initial=RPIO.LOW)
+RPIO.setup(NRF_CE, RPIO.OUT, initial=RPIO.LOW)
 
 for i in range(10):
-#    RPIO.output(NRF_CE, 1)
+    RPIO.output(NRF_CE, 1)
     time.sleep(1)
-#    RPIO.output(NRF_CE, 0)
+    RPIO.output(NRF_CE, 0)
     time.sleep(1)
 
 # reset every channel that has been set up by this program,
 # and unexport interrupt gpio interfaces
-#RPIO.cleanup()
+RPIO.cleanup()
