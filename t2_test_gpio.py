d4409dc94f0f02badc1522e142b28ff85a69835d
Vadym Volokitin <seagsm.trash@gmail.com>
2016-09-17 20:16:02 +0200

    Test2 GPIO

--- t2_test_gpio.py
+++ t2_test_gpio.py
@@ -1,93 +1,74 @@
 #!/usr/bin/python
 
-# Note:
-# Humidity should be monitored at an interval of 3 seconds at least
-# Temperature has no such limitation
-
 import wiringpi as GPIO
-import signal
 
-def initGPIO(pin):
+DQ = 0
+IN = 0
+OUT = 1
+H = 1
+L = 0
+PULL_DN = 1
+PULL_UP = 2
+
+def initGPIO():
     GPIO.wiringPiSetup()
+    GPIO.pullUpDnControl(DQ, PULL_UP)
 
-def timeoutHandler(signum, frame):
-    raise TimeoutError
-
-def untilLow(pin):
+def init18b20():
+    GPIO.pinMode(DQ, OUT)
+    GPIO.digitalWrite(DQ, H)
+    GPIO.digitalWrite(DQ, L)
+    GPIO.delayMicroseconds(480) # 480-960(us)
+    GPIO.pinMode(DQ, IN)
+    GPIO.delayMicroseconds(30) # 15-60(us)
     while True:
-        if GPIO.digitalRead(pin) == 0:
+        if GPIO.digitalRead(DQ) == H:
             break;
+    GPIO.delayMicroseconds(150) # 60-240(us)
+    GPIO.digitalWrite(DQ, H)
 
-def untilHigh(pin):
-    while True:
-        if GPIO.digitalRead(pin) == 1:
-            break;
-
-def readData(pin):
-    hum = 0
-    temp = 0
-    crc = 0
-    GPIO.pinMode(pin, 1)
-    GPIO.digitalWrite(pin, 1)
-    GPIO.digitalWrite(pin, 0)
-    GPIO.delay(20)
-    GPIO.digitalWrite(pin, 1)
-    GPIO.pinMode(pin, 0)
-    GPIO.delayMicroseconds(40)
-    if GPIO.digitalRead(pin) == 0:
-        untilHigh(pin)
-        for i in range(16):
-            hum <<= 1
-            untilLow(pin)
-            untilHigh(pin)
-            GPIO.delayMicroseconds(28)
-            hum += GPIO.digitalRead(pin)
-        for i in range(16):
-            temp <<= 1
-            untilLow(pin)
-            untilHigh(pin)
-            GPIO.delayMicroseconds(28)
-            temp += GPIO.digitalRead(pin)
-        for i in range(8):
-            crc <<= 1
-            untilLow(pin)
-            untilHigh(pin)
-            GPIO.delayMicroseconds(28)
-            crc += GPIO.digitalRead(pin)
-        return [True, hum, temp, crc]
-    else:
-        return [False, hum, temp, crc]
-
-def getTempAndHum(pin):
-    initGPIO(pin)
-    signal.signal(signal.SIGALRM, timeoutHandler)
-    while True:
-        try:
-            signal.alarm(2) # avoid hang or no sensor
-            [isSuccess, humdata, tempdata, crcdata] = readData(pin)
-            signal.alarm(0)
-        except TimeoutError:
-            print("Error: no sensor device or hang")
-            exit(1)
-        if isSuccess == False: # avoid no sensor response
-            continue
-        humInt = (humdata & 0xFF00) >> 8
-        humDec = (humdata & 0xFF)
-        tempInt = (tempdata & 0xFF00) >> 8
-        tempDec = (tempdata & 0xFF)
-        crc = crcdata & 0xFF
-        datasum = (humInt + humDec + tempInt + tempDec) & 0xFF
-        if crc == datasum:
-            return [humInt, tempInt]
+def writeByte(data):
+    GPIO.pinMode(DQ, OUT)
+    for i in range(8):
+        GPIO.digitalWrite(DQ, L)
+        if data & (1 << i):
+            GPIO.digitalWrite(DQ, H)
         else:
-            continue
+            GPIO.digitalWrite(DQ, L)
+        GPIO.delayMicroseconds(30)
+
+def readByte():
+    data = 0;
+    for i in range(8):
+        GPIO.pinMode(DQ, OUT)
+        GPIO.digitalWrite(DQ, L)
+        GPIO.digitalWrite(DQ, H)
+        GPIO.pinMode(DQ, IN)
+        GPIO.delayMicroseconds(15)
+        if GPIO.digitalRead(DQ) == H:
+            data |= (1 << i);
+        else:
+            data &= ~(1 << i);
+        GPIO.delayMicroseconds(30)
+    return data
+
+def getTemp():
+    initGPIO()
+    init18b20()
+    writeByte(0xcc) # skip ROM
+    writeByte(0x44) # convert temperature
+    init18b20()
+    writeByte(0xcc) # skip ROM
+    writeByte(0xbe) # read scrachpad
+    tempL = readByte()
+    tempH = readByte()
+    temp = ((tempH << 4) & 0xF0) + (tempL >> 4) + (tempL & 0x0F) / 16
+    return temp
 
 def main():
-    nGPIO = 0
-    [humidity, temperature] = getTempAndHum(nGPIO)
-
+    temp = getTemp();
+    print("Temperature is %f °C" % temp)
 
 if __name__ == "__main__":
     main()
-
 
