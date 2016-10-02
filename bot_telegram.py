df3f056611bfd83259d46cdeb29399e9a8cb9cb5
Vadym Volokitin <seagsm.trash@gmail.com>
2016-10-03 00:22:00 +0200

    refactoring 1

--- bot_telegram.py
+++ bot_telegram.py
@@ -55,43 +55,46 @@ def signal_term_handler(signal, frame):
 
 
 def handle(msg):
+
     global hum_timestamp
-    chat_id = msg['from']['id']
-    print chat_id
-    command = msg['text']
-    if command == '/on':
-        #GPIO.setmode(GPIO.BOARD)
-        #GPIO.setup(11, GPIO.OUT)
-        #GPIO.output(11,1)
-        bot.sendMessage(chat_id,str('Okey On!'))
+    try:
+        chat_id = msg['from']['id']
+        print chat_id
+        command = msg['text']
+        if command == '/on':
+            #GPIO.setmode(GPIO.BOARD)
+            #GPIO.setup(11, GPIO.OUT)
+            #GPIO.output(11,1)
+            bot.sendMessage(chat_id,str('Okey On!'))
 
-    elif command == '/off':
-        #GPIO.setmode(GPIO.BOARD)
-        #GPIO.setup(11, GPIO.OUT)
-        #GPIO.output(11,0)
-        #GPIO.cleanup()
-        bot.sendMessage(chat_id,str('Okey Off!'))
+        elif command == '/off':
+            #GPIO.setmode(GPIO.BOARD)
+            #GPIO.setup(11, GPIO.OUT)
+            #GPIO.output(11,0)
+            #GPIO.cleanup()
+            bot.sendMessage(chat_id,str('Okey Off!'))
 
-    elif command == '/start':
-        bot.sendMessage(chat_id, str('Hi! I am TowerGarden number 1 !\n You can use commands:\n /temp - show temperature \n /level - show water level \n /image - show webcamera image \n/hum - show humidity'))
-    elif command == '/help':
-        bot.sendMessage(chat_id, str('You can use commands:\n /temp - show temperature \n /level - show water level \n /image - show webcamera image \n/hum - show humidity'))
-    elif command == '/temp':
-        bot.sendMessage(chat_id, 'temp: {:.1f} C'.format(temp_sensor.read_temperature(0)))
-    elif command == '/level':
-        bot.sendMessage(chat_id, 'water_level is: {:.1f} %'.format(water_level.get_raw_distance_blocked()))
-    elif command == '/time':
-        bot.sendMessage(chat_id,'time now: ' + str(datetime.datetime.now()))
-    elif command == '/hum':
-        bot.sendMessage(chat_id, 'humidity now: {:.1f} %\ntemperature now: {:.1f} C\ntime pass: {:.1f} sec '.format(humidity,temperature,hum_timestamp))
-        hum_timestamp = -1
-    elif command == '/image':
-        # Catch image from webcamera:
-        call(["fswebcam", "-r", "1600x1000","--no-timestamp","--no-banner" , "/home/pi/test/current.jpg"])
-        image_file = open('/home/pi/test/current.jpg', 'rb')
-        bot.sendPhoto(chat_id,image_file,str(datetime.datetime.now()))
-        image_file.close()
-
+        elif command == '/start':
+            bot.sendMessage(chat_id, str('Hi! I am TowerGarden number 1 !\n You can use commands:\n /temp - show temperature \n /level - show water level \n /image - show webcamera image \n/hum - show humidity'))
+        elif command == '/help':
+            bot.sendMessage(chat_id, str('You can use commands:\n /temp - show temperature \n /level - show water level \n /image - show webcamera image \n/hum - show humidity'))
+        elif command == '/temp':
+            bot.sendMessage(chat_id, 'temp: {:.1f} C'.format(temp_sensor.read_temperature(0)))
+        elif command == '/level':
+            bot.sendMessage(chat_id, 'water_level is: {:.1f} %'.format(water_level.get_raw_distance_blocked()))
+        elif command == '/time':
+            bot.sendMessage(chat_id,'time now: ' + str(datetime.datetime.now()))
+        elif command == '/hum':
+            bot.sendMessage(chat_id, 'humidity now: {:.1f} %\ntemperature now: {:.1f} C\ntime pass: {:.1f} sec '.format(humidity,temperature,hum_timestamp))
+            hum_timestamp = -1
+        elif command == '/image':
+            # Catch image from webcamera:
+            call(["fswebcam", "-r", "1600x1000","--no-timestamp","--no-banner" , "/home/pi/test/current.jpg"])
+            image_file = open('/home/pi/test/current.jpg', 'rb')
+            bot.sendPhoto(chat_id,image_file,str(datetime.datetime.now()))
+            image_file.close()
+    except KeyboardInterrupt:
+        print "Good bye handler"
 
 # setup signal handler
 signal.signal(signal.SIGTERM, signal_term_handler)
